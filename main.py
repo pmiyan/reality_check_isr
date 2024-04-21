d0ff21cdd428a719e448670f5840bc87836c130d
pmiyan <pmiyan@tamu.edu>
2024-04-21 17:07:59 -0500

    Added brave search. Resturctured the code

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-# This is a sample Python script.
+from brave_search import get_news_documents
+from retrieval import retrieve_url_data
+from similarity import get_similarity
+from dotenv import load_dotenv
+load_dotenv()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from openai import OpenAI
+client = OpenAI()
 
-
-def verify_input(user_query):
-
+def process_user_query(user_query):
+    query_docs = get_news_documents(user_query, client)
+    doc_urls = [doc["url"] for doc in query_docs]
+    for url in doc_urls:
+        document_text = retrieve_url_data(url)
+        response = get_similarity(user_query, document_text, client)
+        print(response)
     return True
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    user_query = input("Enter your statement to verify it:")
-    print(verify_input(user_query))
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # user_query = input("Enter your statement to verify it:")
+    user_query = "Travis Kelce plays for the state of Kansas"
+    process_user_query(user_query)
--- retrieval.py
+++ retrieval.py
@@ -1,30 +1,24 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-from llama_index.core import VectorStoreIndex, download_loader
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.readers.web import BeautifulSoupWebReader
-
-embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-Settings.embedding = embed_model
-Settings.llm = None
-
-loader = BeautifulSoupWebReader()
-
 import requests
 
-def custom_get(url, *args, **kwargs):
-    headers = kwargs.get("headers", {})
-    headers["User-Agent"] = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/123.0.0.0 Safari/537.36"
-    kwargs["headers"] = headers
-    return original_get(url, *args, **kwargs)
+def retrieve_url_data(url):
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.embedding = embed_model
+    Settings.llm = None
+    loader = BeautifulSoupWebReader()
 
-original_get = requests.get
+    def custom_get(url, *args, **kwargs):
+        headers = kwargs.get("headers", {})
+        headers["User-Agent"] = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/123.0.0.0 Safari/537.36"
+        kwargs["headers"] = headers
+        return original_get(url, *args, **kwargs)
 
-requests.get = custom_get
+    original_get = requests.get
 
-url_list = ["https://www.nytimes.com/2024/04/19/style/taylor-swift-lyrics-tortured-poets-department.html", "https://apnews.com/article/gaza-israel-aid-pier-us-military-e988256270fc0067d2f5524fd542a475"]
-for url in url_list:
+    requests.get = custom_get
+
     documents = loader.load_data(urls=[url])
-    print(documents[0].text)
+    url_text = documents[0].text
+    return url_text
